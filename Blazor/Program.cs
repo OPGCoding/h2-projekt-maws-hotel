7bc77a80a3c222f04ca64b7af1b37eb1ec1a050a
RNU4 <malt2592@gmail.com>
2024-08-28 12:06:20 +0200

    DB

--- Blazor/Program.cs
+++ Blazor/Program.cs
@@ -1,27 +1,54 @@
 using Blazor.Components;
+using Blazor.Services;
+using Microsoft.AspNetCore.Authentication;
+using System.Net.NetworkInformation;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+public class Program
 {
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        //GetAllUsedBooks from the Postgres DB
+
+        IConfiguration Configuration = builder.Configuration;
+        var connectionString = Configuration.GetConnectionString("DefaultConnection") ?? Environment.GetEnvironmentVariable("DefaultConnection");
+        builder.Services.AddSingleton<DatabaseService>(sp => new DatabaseService(connectionString));
+
+        // AuthenticationService Registration
+        // Register AuthenticationService as scoped to be consistent with Blazor server's scoped lifecycle for components.
+        builder.Services.AddAuthentication("Cookies")
+            .AddCookie("Cookies", options =>
+            {
+                // Configure your authentication options here
+            });
+
+        // Register AppState
+        builder.Services.AddScoped<AppState>();
+
+
+        // Add services to the container.
+        builder.Services.AddRazorComponents()
+            .AddInteractiveServerComponents();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseStaticFiles();
+        app.UseAntiforgery();
+
+        app.MapRazorComponents<App>()
+            .AddInteractiveServerRenderMode();
+
+        app.Run();
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseStaticFiles();
-app.UseAntiforgery();
-
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
-
-app.Run();
---
+++ Blazor/Services/DatabaseService.cs
@@ -0,0 +1,85 @@
+﻿using Blazor.Components.Pages;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Blazor.Services
+{
+    public class DatabaseService
+    {
+        private readonly string connectionString;
+
+        public DatabaseService(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        /*
+        public List<UsedBooks> GetBooksFromSql(string sql)
+        {
+            List<UsedBooks> allProducts = new List<UsedBooks>();
+            using (var connection = new NpgsqlConnection(connectionString))
+            {
+                connection.Open();
+                using (var command = new NpgsqlCommand(sql, connection))
+                {
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            allProducts.Add(new UsedBooks
+                            {
+                                Id = Convert.ToInt32(reader["Id"]),
+                                Title = reader["Title"].ToString(),
+                                Author = reader["Author"].ToString(),
+                                Condition = reader["Condition"].ToString(),
+                                Category = reader["Category"].ToString(),
+                                Price = reader.IsDBNull(reader.GetOrdinal("Price")) ? 0.0m : Convert.ToDecimal(reader["Price"]),
+                                ImagePath = reader["ImagePath"].ToString(),
+                                Language = reader["Language"].ToString(),
+                                ReleaseDate = reader.GetDateTime(reader.GetOrdinal("ReleaseDate")),
+                                Format = reader["Format"].ToString(),
+                                ISBN = reader.IsDBNull(reader.GetOrdinal("ISBN")) ? 0L : Convert.ToInt64(reader["ISBN"]),
+                                Weight = reader.IsDBNull(reader.GetOrdinal("Weight")) ? 0.0f : Convert.ToSingle(reader["Weight"]),
+                                Pages = reader.IsDBNull(reader.GetOrdinal("Pages")) ? 0 : Convert.ToInt32(reader["Pages"]),
+                                Description = reader["Description"].ToString(),
+                                Type = reader["Type"].ToString(),
+                            });
+                        }
+                    }
+                }
+            }
+            return allProducts;
+        }
+        */
+
+        public int ExecuteSql(string sql)
+        {
+
+            var rowsaffected = -1;
+            using (var connection = new NpgsqlConnection(connectionString))
+            {
+                connection.Open();
+                using (var command = new NpgsqlCommand(sql, connection))
+                {
+                    var result = command.ExecuteScalar();
+
+                    if (result != null && result != DBNull.Value)
+                    {
+                        rowsaffected = Convert.ToInt32(result);
+                    }
+                }
+            }
+            return rowsaffected;
+        }
+    }
+
+    
+
+}
+
+
+
