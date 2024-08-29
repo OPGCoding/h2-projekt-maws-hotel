9b16f9790beb06a1f88fbc9862a330df8c6d4a85
sesi0001 <whir_slicers0w@icloud.com>
2024-08-29 10:58:39 +0200

    in progress

--- DomainModels/Profile.cs
+++ DomainModels/Profile.cs
@@ -9,24 +9,24 @@ namespace DomainModels
 
         [Required]
         [MaxLength(255)]
-        public string Name { get; set; }
+        public string Name { get; set; } = string.Empty;
 
         [Required]
         [EmailAddress]
         [MaxLength(255)]
-        public string Email { get; set; }
+        public string Email { get; set; } = string.Empty;
 
         [Required]
         [MaxLength(255)]
-        public string Password { get; set; }
+        public string Password { get; set; } = string.Empty;
 
         public DateTime? Birthday { get; set; }
 
         [MaxLength(255)]
-        public string Address { get; set; }
+        public string? Address { get; set; }
 
         [MaxLength(20)]
-        public string PhoneNumber { get; set; }
+        public string? PhoneNumber { get; set; }
 
         public bool Administrator { get; set; } = false;
     }
